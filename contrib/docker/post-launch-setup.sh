9028b009877d5936d02cb57733f9c63eba778319
Stefano Semeria <stefano.semeria@gmail.com>
2021-02-11 01:46:00 +0100

    new post launch setup script fixed for 2.9.1

---
+++ contrib/docker/post-launch-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+#db setup
+docker exec ckan /usr/local/bin/ckan -c /etc/ckan/production.ini datastore set-permissions | docker exec -i db psql -U ckan
+
+#enable datastore and pusher
+docker exec ckan sed -i 's/ckan.plugins = stats text_view image_view recline_view/ckan.plugins = stats text_view image_view recline_view datastore datapusher/' /etc/ckan/production.ini
+
+#enable datapusher options
+docker exec ckan sed -i 's/#ckan.datapusher.formats/ckan.datapusher.formats/' /etc/ckan/production.ini
+
+#create admin user
+docker exec -it ckan /usr/local/bin/ckan -c /etc/ckan/production.ini sysadmin add odiadmin
+
+docker-compose restart ckan
+docker-compose restart db
